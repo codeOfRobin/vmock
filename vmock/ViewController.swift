0ef81b24152b63e0fc2134aab5e0f1c8bdc95779
Robin Malhotra <malhotra.robin@yahoo.co.in>
2015-12-02 21:26:58 +0530

    done 2

--- vmock/ViewController.swift
+++ vmock/ViewController.swift
@@ -14,22 +14,49 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        tableView.frame = CGRectMake(0, 300, view.frame.width, view.frame.height)
+        tableView.frame = CGRectMake(0, 0, view.frame.width, view.frame.height)
         tableView.backgroundColor = UIColor.clearColor()
         tableView.backgroundView?.backgroundColor = UIColor.clearColor()
         tableView.dataSource = self
         tableView.delegate = self
         tableView.registerClass(MyTableViewCell.self, forCellReuseIdentifier: "cell")
-        tableView.scrollEnabled = false
-        let pg = UIPanGestureRecognizer(target: self, action: "panned:")
+        tableView.contentInset.top = 300
+        let scroller = UIScrollView(frame: CGRectMake(0,0,view.frame.width,300))
+        scroller.backgroundColor = UIColor.clearColor()
+        scroller.contentSize = CGSizeMake(1000, 1000)
+//        view.addSubview(scroller)
+        
+        let webView = UIWebView(frame: scroller.frame)
+        let path = NSBundle.mainBundle().pathForResource("cv", ofType: "pdf")
+        let url = NSURL(fileURLWithPath: path!)
+        let url2 = NSURL(string: "http://www.cse.iitd.ernet.in/~amitk/SemI-2015/tut9.pdf")
+        let req = NSURLRequest(URL: url2!)
+        webView.loadRequest(req)
+        webView.scalesPageToFit = true
+        
+        
+        
+        let img = UIImageView(frame: CGRectMake(100, 100, 100, 100))
+        img.image = UIImage(named: "screen")
+        scroller.addSubview(webView)
+//        tableView.backgroundView = webView
+        tableView.backgroundView = webView
+        webView.userInteractionEnabled = true
+        webView.scrollView.scrollEnabled = true
+
+        
+        
+        
+        let pg = UITapGestureRecognizer(target: self, action: "panned:")
         pg.delegate = self
-        tableView.addGestureRecognizer(pg)
+        scroller.addGestureRecognizer(pg)
+
         view.addSubview(tableView)
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return 44
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 100
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
@@ -44,59 +71,83 @@ class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate
     
     func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath)
     {
-        cell.backgroundColor = UIColor.redColor()
         cell.textLabel?.text = String(indexPath.row)
         cell.detailTextLabel?.text = "SDAfsadfhkasdfnkjsa"
     }
     
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        
+    }
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
     {
         let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath:indexPath) as! MyTableViewCell
+        let but = UIButton(frame: CGRectMake(300,0,100,100))
+        but.titleLabel?.text = "dsaf"
+        but.backgroundColor = UIColor.redColor()
+        cell.addSubview(but)
+        but.addTarget(self, action: "tapped:", forControlEvents: .TouchUpInside)
         return cell
+    }
+    func tapped(sender:UIButton)
+    {
+        let cell = sender.superview as! MyTableViewCell
+        print(tableView.indexPathForCell(cell)?.row)
+        
+    }
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print(indexPath.row)
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-    func scrollViewDidEndDragging(scrollView: UIScrollView, willDecelerate decelerate: Bool)
-    {
-        if(tableView.scrollEnabled == true && scrollView.contentOffset.y < -100)
-        {
-
-            tableView.gestureRecognizers![2].enabled = true
-            tableView.scrollEnabled = false
-            UIView.animateWithDuration(0.7, animations: { () -> Void in
-                self.tableView.frame = CGRectMake(0, 300, self.view.frame.width, self.view.frame.height)
-                self.tableView.transform = CGAffineTransformMakeTranslation(0, 0)
-            })
-
-        }
-    }
-    
+//
+//    func scrollViewDidEndDragging(scrollView: UIScrollView, willDecelerate decelerate: Bool)
+//    {
+//        if(tableView.scrollEnabled == true && scrollView.contentOffset.y < -100)
+//        {
+//
+//            tableView.gestureRecognizers![2].enabled = true
+//            tableView.scrollEnabled = false
+//            UIView.animateWithDuration(0.7, animations: { () -> Void in
+//                self.tableView.frame = CGRectMake(0, 300, self.view.frame.width, self.view.frame.height)
+//                self.tableView.transform = CGAffineTransformMakeTranslation(0, 0)
+//                self.latestTransform = 0
+//            })
+//
+//        }
+//    }
+//    
     func panned(sender:UIPanGestureRecognizer)
     {
-        print(sender.velocityInView(view))
-        if(sender.velocityInView(view).y < -200)
-        {
-            sender.enabled = false
-            tableView.scrollEnabled = true
-            UIView.animateWithDuration(0.7, animations: { () -> Void in
-                self.tableView.frame = CGRectMake(0, 0, self.view.frame.width, self.view.frame.height)
-                self.tableView.transform = CGAffineTransformMakeTranslation(0, 0)
-            })
-        }
-        if(sender.state == UIGestureRecognizerState.Ended)
-        {
-            latestTransform += sender.translationInView(view).y
-        }
-        else
-        {
-            tableView.transform = CGAffineTransformMakeTranslation(0, sender.translationInView(view).y+latestTransform)
-        }
-        print(latestTransform)
+        print("tap that pg")
+//        
+//        if(latestTransform+sender.translationInView(view).y < -100)
+//        {
+//            self.tableView.scrollEnabled = true
+//            sender.enabled = false
+//        }
+        
+        
+//        if(sender.velocityInView(view).y < -1500)
+//        {
+//            sender.enabled = false
+//            tableView.scrollEnabled = true
+//            UIView.animateWithDuration(0.7, animations: { () -> Void in
+//                self.tableView.frame = CGRectMake(0, 0, self.view.frame.width, self.view.frame.height)
+//                self.tableView.transform = CGAffineTransformMakeTranslation(0, 0)
+//            })
+//        }
+//        if(sender.state == UIGestureRecognizerState.Ended && sender.translationInView(view).y > 0)
+//        {
+//            latestTransform += sender.translationInView(view).y
+//        }
+//        else
+//        {
+//            tableView.transform = CGAffineTransformMakeTranslation(0, sender.translationInView(view).y+latestTransform)
+//        }
     }
-    
 
 }
 
@@ -104,7 +155,9 @@ class MyTableViewCell : UITableViewCell {
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String!) {
         super.init(style: UITableViewCellStyle.Subtitle, reuseIdentifier: reuseIdentifier)
+        
     }
+    
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
